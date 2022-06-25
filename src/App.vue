d3e13de8fb65392ca9cae6d1d80934af29586e67
Chuy <chuyalexgt@gmail.com>
2022-06-25 16:26:17 -0500

    header terminado

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,9 @@
 <template>
-  <main font-sans p="x-4 y-10" text="center gray-700 dark:gray-200">
-    <router-view />
+  <main font-sans p="x-4 y-2" text="center gray-700 dark:gray-200">
+    <Header />
+    <section section>
+      <router-view />
+    </section>
     <Footer />
   </main>
 </template>
