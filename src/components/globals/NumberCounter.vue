517bb8f01e6407dcf7fa3e1c7f11c07ba0d96f48
chuyalexgt <70769106+chuyalexgt@users.noreply.github.com>
2022-10-28 13:13:10 -0500

    feat: CountNumber with decimals

--- src/components/globals/NumberCounter.vue
+++ src/components/globals/NumberCounter.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import gsap from 'gsap'
-const props = defineProps<{ number: number; lowRange?: number }>()
+const props = defineProps<{ number: number; lowRange?: number; decimals?: number }>()
 
 const { number, lowRange } = $(props)
 const tweened = reactive({
@@ -20,6 +20,6 @@ watch($$(number), (n) => {
 
 <template>
   <p class="text-xl font-bold" :class="rangeClass">
-    {{ tweened.number.toFixed(0) }}
+    {{ decimals ? tweened.number.toFixed(decimals) : tweened.number }}
   </p>
 </template>
