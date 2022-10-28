6821022d21bc5b8749becb4b92f98e319dd1418a
chuyalexgt <70769106+chuyalexgt@users.noreply.github.com>
2022-10-28 15:29:48 -0500

    fix: correccion de animacion de lista

--- src/components/globals/AnimatedList.vue
+++ src/components/globals/AnimatedList.vue
@@ -3,12 +3,17 @@ import gsap from 'gsap'
 
 const onBeforeEnter = (el: any) => {
   el.style.opacity = 0
+  el.style.left = '20px'
+  el.style.position = 'relative'
+  el.style.color = 'transparent'
 }
 
 const onEnter = (el: any, done: any) => {
   gsap.to(el, {
     opacity: 1,
     height: '',
+    left: '0',
+    color: 'inherit',
     delay: el.dataset.index * 0.1,
     onComplete: done,
   })
@@ -18,6 +23,8 @@ const onLeave = (el: any, done: any) => {
   gsap.to(el, {
     opacity: 0,
     height: '0',
+    left: '20px',
+    color: 'transparent',
     delay: el.dataset.index * 0.1,
     onComplete: done,
   })
@@ -29,7 +36,7 @@ const onLeave = (el: any, done: any) => {
     <TransitionGroup
       tag="section"
       :css="false"
-      class="flex flex-col gap-1"
+      class="flex flex-col gap-1 relative"
       @before-enter="onBeforeEnter"
       @enter="onEnter"
       @leave="onLeave"
